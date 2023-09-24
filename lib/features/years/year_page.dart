0a31d8fb3501c2a9c1a258c1f784a1dc3cd02602
PiotrRogulski <piter.rogulski@gmail.com>
2023-09-24 21:06:57 +0200

    Prepare for day page

--- lib/features/years/year_page.dart
+++ lib/features/years/year_page.dart
@@ -1,4 +1,6 @@
+import 'package:advent_of_code/design_system/widgets/adaptive_list.dart';
 import 'package:advent_of_code/design_system/widgets/scaffold.dart';
+import 'package:advent_of_code/features/tasks/tasks.dart';
 import 'package:flutter/material.dart';
 
 class YearPage extends MaterialPage<void> {
@@ -20,8 +22,40 @@ class YearScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final yearData = allYears[year]!;
+
     return AocScaffold(
+      key: PageStorageKey(year),
       title: 'Year $year',
+      bodySlivers: [
+        SliverAdaptiveList(
+          items: yearData.days.entries,
+          listItemBuilder: (context, entry) {
+            final MapEntry(key: day, value: dayData) = entry;
+            return Card(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              child: ListTile(
+                title: Text('Day $day'),
+                onTap: () {},
+              ),
+            );
+          },
+          gridItemBuilder: (context, entry) {
+            final MapEntry(key: day, value: dayData) = entry;
+            return Card(
+              child: InkWell(
+                onTap: () {},
+                child: Center(
+                  child: Text('Day $day'),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
