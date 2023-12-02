0a90cf12e90d35af317cb98a2e1dc4b3cb8cfbeb
PiotrRogulski <piter.rogulski@gmail.com>
2023-12-02 06:28:12 +0100

    Add day 2/2023

---
+++ lib/features/tasks/y2023/days/d2.dart
@@ -0,0 +1,109 @@
+import 'package:advent_of_code/features/part/part_implementation.dart';
+import 'package:advent_of_code/features/part/part_input.dart';
+import 'package:advent_of_code/features/part/part_output.dart';
+import 'package:advent_of_code/features/years/models/advent_structure.dart';
+import 'package:collection/collection.dart';
+
+typedef _Subset = ({int red, int green, int blue});
+
+typedef _Game = ({
+  int index,
+  List<_Subset> subsets,
+});
+
+typedef _I = ListInput<_Game>;
+typedef _O = NumericOutput<int>;
+
+class Y2023D2 extends DayData<_I> {
+  const Y2023D2() : super(year: 2023, day: 2);
+
+  static final _gameRegex = RegExp(r'Game (?<index>\d+): (?<subsets>.+)');
+  static RegExp _subsetColorEntry(String color) =>
+      RegExp('(?<count>\\d+) $color');
+
+  @override
+  _I parseInput(String rawData) {
+    return ListInput(
+      rawData
+          .trim()
+          .split('\n')
+          .map(_gameRegex.firstMatch)
+          .nonNulls
+          .map(
+            (m) => (
+              index: int.parse(m.namedGroup('index')!),
+              subsets: m
+                  .namedGroup('subsets')!
+                  .split('; ')
+                  .map(
+                    (s) => (
+                      red: int.parse(
+                        _subsetColorEntry('red')
+                                .firstMatch(s)
+                                ?.namedGroup('count') ??
+                            '0',
+                      ),
+                      green: int.parse(
+                        _subsetColorEntry('green')
+                                .firstMatch(s)
+                                ?.namedGroup('count') ??
+                            '0',
+                      ),
+                      blue: int.parse(
+                        _subsetColorEntry('blue')
+                                .firstMatch(s)
+                                ?.namedGroup('count') ??
+                            '0',
+                      ),
+                    ),
+                  )
+                  .toList()
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  @override
+  Map<int, PartImplementation<_I, _O>> get parts => {
+        1: const _P1(),
+        2: const _P2(),
+      };
+}
+
+class _P1 extends PartImplementation<_I, _O> {
+  const _P1() : super(completed: true);
+
+  @override
+  _O runInternal(_I inputData) {
+    return NumericOutput(
+      inputData.values
+          .where(
+            (game) => game.subsets.every(
+              (subset) =>
+                  subset.red <= 12 && subset.green <= 13 && subset.blue <= 14,
+            ),
+          )
+          .map((game) => game.index)
+          .sum,
+    );
+  }
+}
+
+class _P2 extends PartImplementation<_I, _O> {
+  const _P2() : super(completed: true);
+
+  @override
+  _O runInternal(_I inputData) {
+    return NumericOutput(
+      inputData.values
+          .map(
+            (game) =>
+                game.subsets.map((subset) => subset.red).max *
+                game.subsets.map((subset) => subset.green).max *
+                game.subsets.map((subset) => subset.blue).max,
+          )
+          .sum,
+    );
+  }
+}
--- lib/features/tasks/y2023/year.dart
+++ lib/features/tasks/y2023/year.dart
@@ -1,8 +1,10 @@
 import 'package:advent_of_code/features/tasks/y2023/days/d1.dart';
+import 'package:advent_of_code/features/tasks/y2023/days/d2.dart';
 import 'package:advent_of_code/features/years/models/advent_structure.dart';
 
 const year2023 = YearData(
   days: {
     1: Y2023D1(),
+    2: Y2023D2(),
   },
 );
