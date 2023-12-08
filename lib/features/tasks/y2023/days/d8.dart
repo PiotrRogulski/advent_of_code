50eda069301057702c3ca8cf9d0d1ea93064a130
PiotrRogulski <piter.rogulski@gmail.com>
2023-12-08 06:41:14 +0100

    Add day 8/2023

---
+++ lib/features/tasks/y2023/days/d8.dart
@@ -0,0 +1,117 @@
+import 'package:advent_of_code/common/extensions.dart';
+import 'package:advent_of_code/features/part/part_implementation.dart';
+import 'package:advent_of_code/features/part/part_input.dart';
+import 'package:advent_of_code/features/part/part_output.dart';
+import 'package:advent_of_code/features/years/models/advent_structure.dart';
+
+typedef _Node = ({String left, String right});
+typedef _Map = ({List<_Move> moves, Map<String, _Node> nodes});
+
+typedef _I = ObjectInput<_Map>;
+typedef _O = NumericOutput<int>;
+
+class Y2023D8 extends DayData<_I> {
+  const Y2023D8() : super(year: 2023, day: 8);
+
+  static final _nodeRegex =
+      RegExp(r'^(?<label>\w+) = \((?<left>\w+), (?<right>\w+)\)$');
+
+  @override
+  _I parseInput(String rawData) {
+    return ObjectInput(
+      rawData
+          .split('\n\n')
+          .apply((l) => (moveString: l.first, nodeString: l.last))
+          .apply(
+            (t) => (
+              moves: t.moveString.split('').map(_Move.fromSymbol).toList(),
+              nodes: Map.fromEntries(
+                t.nodeString
+                    .split('\n')
+                    .map(_nodeRegex.firstMatch)
+                    .nonNulls
+                    .map(
+                      (m) => MapEntry(
+                        m.namedGroup('label')!,
+                        (
+                          left: m.namedGroup('left')!,
+                          right: m.namedGroup('right')!,
+                        ),
+                      ),
+                    ),
+              ),
+            ),
+          ),
+    );
+  }
+
+  @override
+  Map<int, PartImplementation<_I, _O>> get parts => {
+        1: const _P1(),
+        2: const _P2(),
+      };
+}
+
+class _P1 extends PartImplementation<_I, _O> {
+  const _P1() : super(completed: true);
+
+  @override
+  _O runInternal(_I inputData) {
+    return NumericOutput(
+      _findPathLength(
+        inputData.value,
+        'AAA',
+        isAtEnd: (node) => node == 'ZZZ',
+      ),
+    );
+  }
+}
+
+class _P2 extends PartImplementation<_I, _O> {
+  const _P2() : super(completed: true);
+
+  @override
+  _O runInternal(_I inputData) {
+    return NumericOutput(
+      inputData.value.nodes.keys
+          .where((n) => n.endsWith('A'))
+          .map(
+            (node) => _findPathLength(
+              inputData.value,
+              node,
+              isAtEnd: (node) => node.endsWith('Z'),
+            ),
+          )
+          .reduce((a, b) => (a * b) ~/ a.gcd(b)),
+    );
+  }
+}
+
+enum _Move {
+  left('L'),
+  right('R');
+
+  const _Move(this.symbol);
+
+  factory _Move.fromSymbol(String s) => values.firstWhere((v) => v.symbol == s);
+
+  final String symbol;
+}
+
+int _findPathLength(
+  _Map map,
+  String start, {
+  required bool Function(String node) isAtEnd,
+}) {
+  var node = start;
+  var count = 0;
+  var moveIndex = 0;
+  while (!isAtEnd(node)) {
+    final move = map.moves[moveIndex];
+    final connections = map.nodes[node]!;
+    node = move == _Move.left ? connections.left : connections.right;
+    count++;
+    moveIndex = (moveIndex + 1) % map.moves.length;
+  }
+  return count;
+}
--- lib/features/tasks/y2023/year.dart
+++ lib/features/tasks/y2023/year.dart
@@ -5,6 +5,7 @@ import 'package:advent_of_code/features/tasks/y2023/days/d4.dart';
 import 'package:advent_of_code/features/tasks/y2023/days/d5.dart';
 import 'package:advent_of_code/features/tasks/y2023/days/d6.dart';
 import 'package:advent_of_code/features/tasks/y2023/days/d7.dart';
+import 'package:advent_of_code/features/tasks/y2023/days/d8.dart';
 import 'package:advent_of_code/features/years/models/advent_structure.dart';
 
 const year2023 = YearData(
@@ -16,5 +17,6 @@ const year2023 = YearData(
     5: Y2023D5(),
     6: Y2023D6(),
     7: Y2023D7(),
+    8: Y2023D8(),
   },
 );
