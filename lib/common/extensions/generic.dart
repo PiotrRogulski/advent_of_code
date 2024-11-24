2144410b9c2684c59c1e901b526eb825102a33dc
PiotrRogulski <piter.rogulski@gmail.com>
2024-11-24 21:29:47 +0100

    Refactor spy print

--- lib/common/extensions/generic.dart
+++ lib/common/extensions/generic.dart
@@ -1,11 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:more/more.dart' as more;
 
 extension GenericX<T> on T {
   @Deprecated('Remove after debugging')
   T spy([dynamic Function(T)? selector]) {
     selector ??= (it) => it;
-    // ignore: avoid_print
-    print('>>> ${selector(this)}');
+    debugPrint('>>> ${selector(this)}');
     return this;
   }
 
